52a43344314a238c96a2c47774c8a41dff392c0b
ozll2b <kyr4515@naver.com>
2021-05-02 23:36:19 +0900

    first commit

---
+++ app/src/main/java/com/cookandroid/hanium/MainActivity.java
@@ -0,0 +1,37 @@
+package com.AndroidStudioProjects.hanium;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+public class MainActivity extends AppCompatActivity {
+    RadioGroup rg;
+    LinearLayout info;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        info = findViewById(R.id.info);
+        rg = findViewById(R.id.rg);
+
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton recommendYes = findViewById(R.id.recommendyes);
+                RadioButton recommendNo = findViewById(R.id.recommendno);
+
+                if(recommendYes.isChecked()) info.setVisibility(View.VISIBLE);
+                else info.setVisibility(View.GONE);
+            }
+        });
+
+
+    }
+
+}
