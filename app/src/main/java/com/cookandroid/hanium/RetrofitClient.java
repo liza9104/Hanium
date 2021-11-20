5dd177887f5749cb2c300ea6c5d8a4a844f95742
ozll2b <kyr4515@naver.com>
2021-11-20 21:48:12 +0900

    홈 화면 아이콘 추가

--- app/src/main/java/com/cookandroid/hanium/RetrofitClient.java
+++ app/src/main/java/com/cookandroid/hanium/RetrofitClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private final static String BASE_URL = "http://ec2-54-180-139-142.ap-northeast-2.compute.amazonaws.com:3000";
+    private final static String BASE_URL = "http://ec2-13-124-252-104.ap-northeast-2.compute.amazonaws.com:3000";
     private static Retrofit retrofit = null;
 
     private RetrofitClient() {
